7b953b0e7d10eb95e0d9d8e6e8382a8cd6928fa0
sbutler2901 <sbsoftware@protonmail.com>
2017-12-09 12:53:15 -0500

    The extension's settings page now saves and restores previously selected options with some error handling

--- options/options.js
+++ options/options.js
@@ -1,30 +1,121 @@
+
 function ualog(content) {
-  console.log("UASpoofer: " + content);
+    console.log("UASpoofer: " + content);
+}
+
+function onError(error) {
+    ualog('Error: ${error}');
+}
+
+function printSavedOptions() {
+    if ( shouldDebug ) {
+        ualog("Printing saved options");
+        ualog("Storage set: disabled: " + document.querySelector("#disabled").checked);
+        ualog("Storage set: user agents: " + document.querySelector("#ua-textarea").value);
+        ualog("Storage set: should freq: " + document.querySelector("#chng-freq-chk").checked);
+        ualog("Storage set: freq interval: " + document.querySelector("#chng-freq-time").value);
+        ualog("Storage set: same device: " + document.querySelector("#same-device").checked);
+        ualog("Storage set: same os: " + document.querySelector("#same-os").checked);
+        ualog("Storage set: same browser: " + document.querySelector("#same-browser").checked);
+        ualog("End");
+    }
 }
 
 function saveOptions(e) {
-  e.preventDefault();
-  browser.storage.local.set({
-    color: document.querySelector("#color").value,
-    user_agents: document.querySelector("#ua-textarea").value
-  });
-  ualog("textarea: " + document.querySelector("#ua-textarea").value);
-  alert("textarea: " + document.querySelector("#ua-textarea").value);
+    e.preventDefault();
+    browser.storage.local.set({
+        disabled: document.querySelector("#disabled").checked,
+        user_agents: document.querySelector("#ua-textarea").value,
+        should_change_freq: document.querySelector("#chng-freq-chk").checked,
+        change_freq_time: document.querySelector("#chng-freq-time").value,
+        should_only_use_same_device: document.querySelector("#same-device").checked,
+        should_only_use_same_os: document.querySelector("#same-os").checked,
+        should_only_use_same_browser: document.querySelector("#same-browser").checked
+    }).then(printSavedOptions, onError);
 }
 
 function restoreOptions() {
 
-  function setCurrentChoice(result) {
-    document.querySelector("#color").value = result.color || "blue";
-  }
+    browser.storage.local.get("disabled").then((res) => {
+        if ( shouldDebug )
+            ualog("Storage Get: disabled: " + res.disabled);
 
-  function onError(error) {
-    console.log(`Error: ${error}`);
-  }
+        if ( res.disabled === undefined )
+            document.querySelector("#disabled").checked = defaultIsDisabled;
+        else
+            document.querySelector("#disabled").checked = res.disabled;
+    }, onError);
 
-  var getting = browser.storage.local.get("color");
-  getting.then(setCurrentChoice, onError);
+    browser.storage.local.get("user_agents").then((res) => {
+        if ( shouldDebug )
+            ualog("Storage Get: user agents: " + res.user_agents);
+
+        if ( res.user_agents === undefined )
+            document.querySelector("#ua-textarea").value = defaultUserAgents;
+        else
+            document.querySelector("#ua-textarea").value = res.user_agents;
+    }, onError);
+
+    browser.storage.local.get("should_change_freq").then((res) => {
+        if ( shouldDebug )
+            ualog("Storage Get: should freq: " + res.should_change_freq);
+
+        if ( res.should_change_freq === undefined )
+            document.querySelector("#chng-freq-chk").checked = defaultShouldChangeFreq;
+        else
+            document.querySelector("#chng-freq-chk").checked = res.should_change_freq;
+    }, onError);
+
+    browser.storage.local.get("change_freq_time").then((res) => {
+        if ( shouldDebug )
+            ualog("Storage Get: freq interval: " + res.change_freq_time);
+
+        if ( res.change_freq_time === undefined )
+            document.querySelector("#chng-freq-time").value = defaultChangeFreq;
+        else
+            document.querySelector("#chng-freq-time").value = res.change_freq_time;
+    }, onError);
+
+    browser.storage.local.get("should_only_use_same_device").then((res) => {
+        if ( shouldDebug )
+            ualog("Storage Get: same device: " + res.should_only_use_same_device);
+
+        if ( res.should_only_use_same_device === undefined )
+            document.querySelector("#same-device").checked = defaultShouldUseSameDevice;
+        else
+            document.querySelector("#same-device").checked = res.should_only_use_same_device;
+    }, onError);
+
+    browser.storage.local.get("should_only_use_same_os").then((res) => {
+        if ( shouldDebug )
+            ualog("Storage Get: same os: " + res.should_only_use_same_os);
+
+        if ( res.should_only_use_same_os === undefined )
+            document.querySelector("#same-os").checked = defaultShouldUseSameOS;
+        else
+            document.querySelector("#same-os").checked = res.should_only_use_same_os;
+    }, onError);
+
+    browser.storage.local.get("should_only_use_same_browser").then((res) => {
+        if ( shouldDebug )
+            ualog("Storage Get: same browser: " + res.should_only_use_same_browser);
+
+        if ( res.should_only_use_same_browser === undefined )
+            document.querySelector("#same-browser").checked = defaultShouldUseSameBrowser;
+        else
+            document.querySelector("#same-browser").checked = res.should_only_use_same_browser;
+    }, onError);
 }
+
+// tmp ua
+const shouldDebug = false;
+const defaultIsDisabled = false;
+const defaultUserAgents = "Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16";
+const defaultShouldChangeFreq = false;
+const defaultChangeFreq = 30;
+const defaultShouldUseSameDevice = true;
+const defaultShouldUseSameOS = false;
+const defaultShouldUseSameBrowser = false;
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
 document.querySelector("form").addEventListener("submit", saveOptions);
