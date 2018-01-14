2eb775af04b191ed37bf95f26c9763d11d918b16
sbutler2901 <sbsoftware@protonmail.com>
2018-01-14 12:26:08 -0500

    Parser now retrieves user agents using HTTPS, extracts them, and then produces the JS file

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	cd parser && node parser.js
+	cd parser && node parser.js && mv ./defaultUserAgents.js ../helpers
 
 package:
 	rm -f UAPhantom.zip && zip -r UAPhantom.zip globals.js main.js manifest.json helpers icons options popup -x icons/Phantom.xcf \*.DS_Store
--- parser/parser.js
+++ parser/parser.js
@@ -1,15 +1,17 @@
 'use strict';
 
-var UAParser = require('ua-parser-js');
 var fs = require('fs');
 var readline = require('readline');
 var https = require('https');
+var htmlparser = require('htmlparser');
+var UAParser = require('ua-parser-js');
 
 
 //********************* Local Variables *********************
 
 let uaFilePath = "useragents.txt";
-let uaJSFilePath = "../helpers/defaultUserAgents.js";
+let uaJSFilePath = "defaultUserAgents.js";
+//let uaJSFilePath = "../helpers/defaultUserAgents.js";
 let uaRawHTMLPath = "useragents.html";
 
 var parser = new UAParser();
@@ -23,34 +25,6 @@ init();
 
 
 //********************* Function Declarations *********************
-
-
-function extractUserAgents(callback) {
-
-}
-
-function getUserAgents(callback) {
-    var options = {
-      host: 'techblog.willshouse.com',
-      port: 443,
-      path: '/2012/01/03/most-common-user-agents/'
-    };
-
-    var uaRawHtml = fs.createWriteStream(uaRawHTMLPath);
-
-    https.get(options, function(resp) {
-        resp.setEncoding('utf8');
-        resp.on('data', function(chunk) {
-            uaRawHtml.write(chunk);
-        });
-        resp.on('end', function () {
-            uaRawHtml.close();
-            callback();
-        });
-    }).on("error", function(e){
-        console.log("Error getting user agents " + e.message);
-    });
-}
 
 // Called each time a line is read
 function lineReaderLine(line) {
@@ -69,15 +43,15 @@ function lineReaderClose() {
     var fileString = "defaultUAs = " + JSON.stringify(uajsonarr) +  ";";
     fs.writeFile(uaJSFilePath, fileString, function (err) {
         if (err) {
-            console.log(err);
+            console.log("Error creating " + uaJSFilePath + ": " + err);
         } else {
-            console.log("Success writing file!");
+            console.log(uaJSFilePath + " has been created");
         }
     });
 }
 
 // Performs parsing of user agent file
-function lineReader() {
+function createUAJS() {
     var lineReader = readline.createInterface({
       input: fs.createReadStream(uaFilePath)
     });
@@ -86,10 +60,52 @@ function lineReader() {
     lineReader.on('close', lineReaderClose);
 }
 
+// Performs the https request getting html containing user agents and extracts them
+function getUserAgents(callback) {
+    var options = {
+      host: 'techblog.willshouse.com',
+      port: 443,
+      path: '/2012/01/03/most-common-user-agents/'
+    };
+
+    var handler = new htmlparser.DefaultHandler(function (err, dom) {
+        if (err)
+            console.log("Error parsing html: " + err);
+        else {
+            var uaListClass = htmlparser.DomUtils.getElements({ class: "get-the-list" }, dom);
+            fs.writeFile(uaFilePath, uaListClass[0].children[0].data, (err) => {
+                if (err) 
+                    console.log("Error writing " + uaFilePath + ": " + err.message);
+                else {
+                    console.log(uaFilePath + " has been created");
+                    callback();
+                }
+            });
+        }
+    });
+
+    // Uncomment these to write the intermediate HTML file
+    //var uaRawHtml = fs.createWriteStream(uaRawHTMLPath);
+
+    var parser = new htmlparser.Parser(handler);
+
+    https.get(options, function(resp) {
+        resp.setEncoding('utf8');
+        resp.on('data', function(chunk) {
+            parser.parseChunk(chunk);
+            //uaRawHtml.write(chunk);
+        });
+        resp.on('end', function () {
+            //uaRawHtml.close();
+            parser.done();
+        });
+    }).on("error", function (err) {
+        console.log("Error getting user agents using HTTPS: " + err.message);
+    });
+}
+
 function init() {
     getUserAgents(function () {
-        extractUserAgents(function () {
-            lineReader();
-        });
+        createUAJS();
     });
 }
